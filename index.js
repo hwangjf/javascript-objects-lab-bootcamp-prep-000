c3a4fa8931b1c14d956d6a00934b5a5bdab0b62c
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-19 23:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue (object, key, value) {
-  object[key] = value;
-  return object
-}
-
-function updateObjectWithKeyAndValue (object, key, value) {
-  object.assign = ({}, key)
-  return object
-}
