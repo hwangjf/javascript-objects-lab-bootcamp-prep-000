9de234f2f00498bd4940e29c7b1245f7b47dda19
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-20 01:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue (object, key, value) {
-//this function is taking a new key value pair and adding it into an empty target object 
+//this function is taking a new key value pair and adding it into an empty target object without modifying the actual object that is a paramater of the function
   return Object.assign ({}, object, {[key]: value})
 }
 
