6f44748350f3fcb715d389d8b89904bbee97aee4
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-20 01:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ var recipes = {}
 function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign ({}, object, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  
+  return object
+}
+
+function deleteFromObjectByKey (object, key) {
+  
+}
