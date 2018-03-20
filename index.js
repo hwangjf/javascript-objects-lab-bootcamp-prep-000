32501d9e4793db0b68b5c158f4e8f1b5e54e0b68
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-20 01:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue (object, key, value) {
-//this function is taking a new key value pair and adding it into an empty target object without modifying the actual object that is a paramater of the function
+//this function is taking a new key value pair and adding it into an empty target object without modifying the actual object that is a paramater of the function; also adding the object to the empty object
   return Object.assign ({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+//this function is changing the object in order to have the new key value pair added to the object and returning the new object that results in this change
   object[key] = value;
   return object
 }
